80d14ef0f093747b1b8b2917a40ea7601adbec74
Isaac Duarte <isaac.duarte@cloudframe.com>
2023-02-07 18:59:24 -0500

    fix doc

---
+++ doc/implementors/adv2_splitter/codec/trait.AdCodec.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"adv2_splitter":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ doc/source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"adv2_splitter":["",[],["advanced_dupe.rs","codec.rs","macro.rs","main.rs"]]\
+}');
+createSourceSidebar();
